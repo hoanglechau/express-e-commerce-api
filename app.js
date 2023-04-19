32b4ace7e9849b568952d21cbda6c89f5768b2f2
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-04-19 17:05:00 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-console.log('E-Commerce API');
+// invoke the dotenv package in order to allow the use of environment variables
+require("dotenv").config();
+
+// express
+
+const express = require("express");
+const app = express();
+
+// database
+const connectDB = require("./db/connect");
+
+const port = process.env.PORT || 5000;
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`Server is listening on port ${port}...`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
