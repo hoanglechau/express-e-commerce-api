86bb116df96252f7095c85ba235d5a496d24f338
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-04-19 17:16:52 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 // invoke the dotenv package in order to allow the use of environment variables
 require("dotenv").config();
+// package to apply try and catch to all controllers automatically
+require("express-async-errors");
 
 // express
 
@@ -8,6 +10,22 @@ const app = express();
 
 // database
 const connectDB = require("./db/connect");
+
+// middlewares
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
+
+// middleware to allow access to json data
+app.use(express.json());
+
+// routes
+app.get("/", (req, res) => {
+  res.send("e-commerce api");
+});
+
+app.use(notFoundMiddleware);
+// error handler needs to be the last one
+app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 5000;
 const start = async () => {
