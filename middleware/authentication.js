1a60830e0c6b5dc8dcf61727cb09372af6ca3886
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-04-20 15:43:34 +0700

    Refactor authorizePermission middleware

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -17,6 +17,7 @@ const authenticateUser = async (req, res, next) => {
   }
 };
 
+/*
 const authorizePermissions = (req, res, next) => {
   if (req.user.role !== "admin") {
     throw new CustomError.UnauthorizedError(
@@ -24,6 +25,19 @@ const authorizePermissions = (req, res, next) => {
     );
   }
   next();
+};
+*/
+
+// Allow passing in an array of roles that are authorized to access the route
+const authorizePermissions = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      throw new CustomError.UnauthorizedError(
+        "Unauthorized to access this route"
+      );
+    }
+    next();
+  };
 };
 
 module.exports = {
