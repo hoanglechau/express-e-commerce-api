0dad0291abde7c8343aa199529afa5538c6c5168
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-04-23 20:51:18 +0700

    Add Review routes

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ require("dotenv").config();
 require("express-async-errors");
 
 // express
-
 const express = require("express");
 const app = express();
 
@@ -20,6 +19,7 @@ const connectDB = require("./db/connect");
 const authRouter = require("./routes/authRoutes");
 const userRouter = require("./routes/userRoutes");
 const productRouter = require("./routes/productRoutes");
+const reviewRouter = require("./routes/reviewRoutes");
 
 // middlewares
 const notFoundMiddleware = require("./middleware/not-found");
@@ -52,6 +52,7 @@ app.get("/api/v1", (req, res) => {
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/products", productRouter);
+app.use("/api/v1/reviews", reviewRouter);
 
 // 404 Not Found is put after all the routes
 app.use(notFoundMiddleware);
